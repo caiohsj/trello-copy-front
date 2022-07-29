561a2af1ebcab123639d8370e715249c1459273f
Caio Henrique <caiocoxim10@gmail.com>
2022-07-28 22:45:04 -0800

    [UPDATE] improvement

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -4,18 +4,34 @@ export const ApiService: Http = {
   baseUrl: 'http://localhost:3000',
   async get(endpoint: string, query?: any) {
     const response = await fetch(this.baseUrl + endpoint, { method: 'GET', body: query })
-    return response.json()
+    const json = await response.json()
+    return new Promise<any>((resolve, reject) => {
+      if (response.ok) resolve(json)
+      reject(json)
+    })
   },
   async post(endpoint: string, params: any) {
     const response = await fetch(this.baseUrl + endpoint, { method: 'POST', body: JSON.stringify(params), headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
-    return response.json()
+    const json = await response.json()
+    return new Promise<any>((resolve, reject) => {
+      if (response.ok) resolve(json)
+      reject(json)
+    })
   },
   async put(endpoint: string, params: any) {
     const response = await fetch(this.baseUrl + endpoint, { method: 'PUT', body: JSON.stringify(params), headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
-    return response.json()
+    const json = await response.json()
+    return new Promise<any>((resolve, reject) => {
+      if (response.ok) resolve(json)
+      reject(json)
+    })
   },
   async delete(endpoint: string) {
     const response = await fetch(this.baseUrl + endpoint, { method: 'DELETE' })
-    return response.json()
+    const json = await response.json()
+    return new Promise<any>((resolve, reject) => {
+      if (response.ok) resolve(json)
+      reject(json)
+    })
   }
 }
