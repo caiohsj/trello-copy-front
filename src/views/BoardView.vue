540fbcc8dc221aa63eba4d65ebd0c49b550e39ce
Caio Henrique <caiocoxim10@gmail.com>
2022-08-13 20:06:18 -0400

    [NEW] save column

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -2,29 +2,22 @@
   <div
     v-if="boardStore.getBoard"
     :style="background"
-    style="height: calc(100vh - 52px)"
-    class="w-full relative overflow-x-auto"
+    class="board-view w-full relative overflow-x-auto bg-cover bg-no-repeat"
   >
     <div class="absolute bottom-0 top-0 p-4">
-      <button
-        @click="addColumn"
-        class="p-2 mb-4 bg-emerald-500 rounded-md text-gray-200 font-bold hover:opacity-75 transition-all"
-      >
-        Adicionar coluna
-      </button>
-
       <section class="flex gap-2">
         <div
           class="bg-gray-200 rounded-md px-4 pb-2"
           style="min-width: 300px"
-          v-for="column in boardStore.getBoard.columns"
+          v-for="(column, index) in boardStore.getBoard.columns"
           :key="column.id"
         >
           <input
+            @keyup.enter="handleSaveColumn(column, index)"
+            v-model="column.title"
             class="ease-in-out duration-100 w-full focus:border-b-2 border-gray-800 bg-transparent outline-none pt-2 pl-2 font-bold"
             type="text"
             id="inputNameColumn"
-            v-model="column.title"
           />
 
           <div
@@ -35,6 +28,12 @@
             {{ card.title }}
           </div>
         </div>
+        <button
+          @click="addColumn"
+          class="p-2 mb-4 bg-emerald-500 rounded-md text-gray-200 font-bold hover:opacity-75 transition-all"
+        >
+          +
+        </button>
       </section>
     </div>
   </div>
@@ -42,10 +41,13 @@
 
 <script lang="ts" setup>
 import { useBoardStore } from "@/stores/board";
+import { useColumnStore } from "@/stores/column";
+import type { Column } from "@/types/models/Column";
 import { computed } from "vue";
 import { useRoute } from "vue-router";
 
 const boardStore = useBoardStore();
+const columnStore = useColumnStore();
 const route = useRoute();
 
 boardStore.fetchBoard(Number(route.params.id));
@@ -65,6 +67,17 @@ const addColumn = () => {
     id: 0,
     title: "Digite aqui o nome da coluna",
     cards: [],
+    board_id: boardStore.getBoard.id,
   });
 };
+
+const handleSaveColumn = (column: Column, index: number) => {
+  columnStore.saveColumn(column, index);
+};
 </script>
+
+<style lang="scss">
+.board-view {
+  height: calc(100vh - 52px);
+}
+</style>
