b9553a4c4673a6b56ea84701cc23cc38eeb04892
Caio Henrique <caiocoxim10@gmail.com>
2022-08-14 23:26:39 -0400

    [NEW] emit event when clicked in card

--- src/components/CardColumn.vue
+++ src/components/CardColumn.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    @click="emitClicked"
     class="p-2 mb-2 shadow-lg cursor-pointer bg-gray-50 rounded-md mt-2 hover:bg-gray-300"
   >
     <div
@@ -13,7 +14,13 @@
 <script lang="ts" setup>
 import type { Card } from "@/types/models/Card";
 
+const emit = defineEmits(["clicked"]);
+
 const props = defineProps<{
   item: Card;
 }>();
+
+const emitClicked = () => {
+  emit("clicked", props.item);
+};
 </script>
