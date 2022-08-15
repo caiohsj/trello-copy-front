509b067b559cd03882d1d8fba3a3d378dd267394
Caio Henrique <caiocoxim10@gmail.com>
2022-08-14 23:24:53 -0400

    [NEW] card resource

---
+++ src/api/resources/card.ts
@@ -0,0 +1,14 @@
+import http from "@/api/http";
+import type { Card } from "@/types/models/Card";
+
+export const CardResource = {
+  create(card: Card) {
+    return http.post("/api/v1/cards", card);
+  },
+  update(card: Card) {
+    return http.put(`/api/v1/cards/${card.id}`, card);
+  },
+  show(id: number) {
+    return http.get(`/api/v1/cards/${id}`);
+  },
+};
